b0d454dcc6a257ecd4ecd220a333e5fc697f7e96
ArjunDahal99 <dahalarjun409@gmail.com>
2024-05-11 13:33:05 +0545

    donee

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -16,7 +16,7 @@ export const app: Express = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', "https://micro-rec.vercel.app"],
     methods: ['*'],
     credentials: true
 }));
